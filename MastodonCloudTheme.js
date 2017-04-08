1240c7dc01969ac6f8ad37d5a884f5cfc94d25d0
Arthur Lacoste <arthak@gmail.com>
2017-04-08 12:27:42 +0200

    First commit
    This is a simple Greasmonkey script allow you to change the default
    theme "tweetdeck-like" into another clear and beautiful theme.

---
+++ MastodonCloudTheme.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Mastodon cloud theme
+// @namespace    http://arthurlacoste.com
+// @version      0.1
+// @description  Change default Mastodon Theme
+// @author       Arthur Lacoste
+// @match        https://mastodon.social/web/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    
+// espace de commentaire
+GM_addStyle ( " .drawer__inner { background: #c9cfe4 !important; }");
+
+// bord bas des toots
+GM_addStyle ( "  .status {    border-bottom: 1px solid #c9cfe4; } ");
+
+// olones
+GM_addStyle ( ".column, .detailed-status, .detailed-status__action-bar{background: #c9cfe4;}"); 
+
+// couleur du texte du statut
+GM_addStyle ( " .status__content {color: #393f4f  !important;}"); 
+    
+// La scrollbar
+GM_addStyle ( "::-webkit-scrollbar-thumb { background: #8088a6;}");   
+    
+// Couleur de fond
+GM_addStyle ( "body.app-body, body, .ui { background: #e1e4ec !important;}");
+        
+// Recherche
+GM_addStyle ( " .search__input { background: #c9cfe4; color: #9baec8; } ");
+
+// username tooter
+GM_addStyle ( ".status__display-name strong, .account__display-name strong { color: #393f4f; }");
+    
+// liens   
+GM_addStyle ( " .status__content a, .reply-indicator__content a {   color: #8190a6; }");
+    
+// retirer les barres dans le status détaillé
+GM_addStyle ( ".detailed-status__action-bar { border-top:0px;border-bottom: 0x;}");
+    
+// fond profil
+GM_addStyle ( ".account__header>div {background: rgba(201, 207, 228,0.7);}");
+    
+// profil, décompte followers
+GM_addStyle ( ".account__action-bar__tab strong { color: #606984;}");
+                                   
+})();
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+
+# Mastodon Cloud Theme
+
+This is a simple Greasmonkey script allow you to change the default theme "tweetdeck-like" into another clear and beautiful theme.
+
+# Install
+
+On chrome, install the Tampermonkey extension here :
+https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=fr
+
+And copy paste the userscript in the interface.
