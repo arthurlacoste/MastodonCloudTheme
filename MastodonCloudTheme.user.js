3ef047e6798d71c90833792e3866f41c65ccc9c6
Arthur Lacoste <arthak@gmail.com>
2017-04-08 13:46:57 +0200

    erreur statuts détaillés

--- MastodonCloudTheme.user.js
+++ MastodonCloudTheme.user.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  Change default Mastodon Theme
 // @author       Arthur Lacoste
-// @match        https://*/web/*
+// @match        *://*/web/*
+
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -13,16 +14,16 @@
     
     
 // espace de commentaire
-GM_addStyle ( " .drawer__inner { background: #c9cfe4 !important; }");
+GM_addStyle ( ".drawer__inner { background: #c9cfe4 !important; }");
 
 // bord bas des toots
-GM_addStyle ( "  .status {    border-bottom: 1px solid #c9cfe4; } ");
+GM_addStyle ( ".status {    border-bottom: 1px solid #c9cfe4; } ");
 
 // olones
 GM_addStyle ( ".column, .detailed-status, .detailed-status__action-bar{background: #c9cfe4;}"); 
 
 // couleur du texte du statut
-GM_addStyle ( " .status__content {color: #393f4f  !important;}"); 
+GM_addStyle ( ".status__content {color: #393f4f  !important;}"); 
     
 // La scrollbar
 GM_addStyle ( "::-webkit-scrollbar-thumb { background: #8088a6;}");   
@@ -31,22 +32,22 @@ GM_addStyle ( "::-webkit-scrollbar-thumb { background: #8088a6;}");
 GM_addStyle ( "body.app-body, body, .ui { background: #e1e4ec !important;}");
         
 // Recherche
-GM_addStyle ( " .search__input { background: #c9cfe4; color: #9baec8; } ");
+GM_addStyle ( ".search__input { background: #c9cfe4; color: #9baec8; } ");
 
 // username tooter
 GM_addStyle ( ".status__display-name strong, .account__display-name strong { color: #393f4f; }");
     
 // liens   
-GM_addStyle ( " .status__content a, .reply-indicator__content a {   color: #8190a6; }");
+GM_addStyle ( ".status__content a, .reply-indicator__content a {   color: #8190a6; }");
     
-// retirer les barres dans le status détaillé
-GM_addStyle ( ".detailed-status__action-bar { border-top:0px;border-bottom: 0x;}");
-    
+// retirer les barres dans le statuts détaillés
+GM_addStyle ( ".detailed-status__action-bar, .account { border-top:0px;border-bottom: 0px;}");
+   
 // fond profil
 GM_addStyle ( ".account__header>div {background: rgba(201, 207, 228,0.7);}");
     
 // profil, décompte followers
 GM_addStyle ( ".account__action-bar__tab strong { color: #606984;}");
-                                   
+                                 
 })();
 
